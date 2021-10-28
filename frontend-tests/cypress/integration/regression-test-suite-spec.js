51f366508c61d2bcaa1b7c05509048654101ff11
Omar Saifi <omar.saifi@yh.nackademin.se>
2021-10-28 17:43:48 +0200

    Created a .gitignore and test project

---
+++ frontend-tests/cypress/integration/regression-test-suite-spec.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress" />
+
+//This is a test suite
+describe('regression test suite', function(){
+
+    // this is a test case
+    it('perform valid login', function(){
+        cy.visit('http://localhost:3000')
+
+
+    })
+
+})
