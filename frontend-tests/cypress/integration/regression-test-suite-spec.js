c0e34960257400c040c364f55344dbb330022f4e
Omar Saifi <omar.saifi@yh.nackademin.se>
2021-10-28 20:26:16 +0200

    Created login-logout test case

--- frontend-tests/cypress/integration/regression-test-suite-spec.js
+++ frontend-tests/cypress/integration/regression-test-suite-spec.js
@@ -6,6 +6,14 @@ describe('regression test suite', function(){
     // this is a test case
     it('perform valid login', function(){
         cy.visit('http://localhost:3000')
+        cy.title().should('include', 'Hotel')
+        cy.contains('Login')
+        cy.get(':nth-child(1) > input').type('tester01')
+        cy.get(':nth-child(2) > input').type('GteteqbQQgSr88SwNExUQv2ydb7xuf8c')
+        cy.get('.btn').click()
+        cy.contains('Tester Hotel Overview')
+        cy.get('.user > .btn').click()
+        cy.contains('Login')
 
 
     })
